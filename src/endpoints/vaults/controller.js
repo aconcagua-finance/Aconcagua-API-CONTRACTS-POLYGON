b8a3c3ae806ce0de8f3920e7c958c385ab5a0868
Aconcagua-CTO <manuel@aconcagua.finance>
2024-04-10 06:39:09 -0300

    Update controller.js

--- src/endpoints/vaults/controller.js
+++ src/endpoints/vaults/controller.js
@@ -2180,10 +2180,6 @@ const buildSwapsParams = async (swapsData) => {
     );
   }
   const quotes = apiResponse.data;
-  console.log(
-    'buildSwapsParams - swap path is ',
-    encodePath(staticPaths[tokenIn.symbol].tokens, staticPaths[tokenIn.symbol].fees)
-  );
 
   // Armo objetos swap.
   return swapsData
